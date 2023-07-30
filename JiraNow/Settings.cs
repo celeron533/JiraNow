abaa389f0acba5581f84ca07a15ec6ea7e7c2536
celeron533 <zhuyilun8@hotmail.com>
2023-07-30 17:33:23 +0800

    save/load settings

---
+++ JiraNow/Settings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JiraNow
+{
+    internal class Settings
+    {
+        public Settings() { }
+        public static Settings Default
+        {
+            get { return new Settings(); }
+        }
+
+        public string HostString { get; set; }
+        public string CookiesString { get; set; }
+
+    }
+}
---
+++ JiraNow/SettingsManager.cs
@@ -0,0 +1,35 @@
+﻿using JiraNow.Utils;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JiraNow
+{
+    internal class SettingsManager
+    {
+        private const string filename = "settings.json";
+
+        public static void SaveSettings(Settings settings, string filename = filename)
+        {
+            JsonManager.SaveObjectToJsonFile(settings, filename);
+        }
+
+        public static Settings LoadSettings(string filename = filename)
+        {
+            if (!File.Exists(filename))
+            {
+                Settings settings = Settings.Default;
+                SaveSettings(Settings.Default, filename);
+                return settings;
+            }
+            else
+            {
+                return JsonManager.LoadObjectFromJsonFile<Settings>(filename);
+            }
+        }
+    }
+}
